d6eee62baeefaca9af72af1dca19e3346e9d4c58
unknown <andrew12lewis@gmail.com>
2019-06-11 15:29:52 -0700

    feat: basic text header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+		<Header />
 		<h1>Welcome to the CryptoForest website</h1>
 		<p> this works</p>
 	</div>

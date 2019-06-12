51004b1b8bb26391cfdf624757c75c97d54ff1d1
unknown <andrew12lewis@gmail.com>
2019-06-11 17:07:44 -0700

    feat: added footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
 		<Header />
 		<h1>Welcome to the CryptoForest website</h1>
 		<p> this works</p>
+		<Footer />
 	</div>
   );
 }

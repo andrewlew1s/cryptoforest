ba3a27a11023862ec6f6b8e349a5d6e7fd183c63
unknown <andrew12lewis@gmail.com>
2019-06-11 21:10:52 -0700

    fix: display img from API

--- src/components/tokens/tokenDisplay.js
+++ src/components/tokens/tokenDisplay.js
@@ -8,25 +8,33 @@ class TokenDisplay extends React.Component {
 		super(props);
 		this.state = {
 			tokenMetaData: [],
+			imageURLS: []
 		};
 	}
-	componentWillMount() {
+	componentDidMount() {
 		const NUMTOKENS = 7;
 		let metaData = this.state.tokenMetaData;
+		let imgURLS = this.state.imageURLS;
 		for(var i=1; i <=NUMTOKENS; i++){
 			axios.get('https://crypto-forest.herokuapp.com/api/creature/' + i)
+			// eslint-disable-next-line no-loop-func
 			.then(res => {
 				const tokenMetaData = res.data;
 				metaData = metaData.concat(tokenMetaData);
 				this.setState({tokenMetaData: metaData});
-				console.log(this.state)
-			})
+					let imgs = [];
+					imgs = this.state.tokenMetaData.map((token, i, tokenMetaData) => {
+						return token.image
+					});
+					this.setState({imageURLS: imgs})
+				}
+			)
 		}
 	}
 	render() {
 		return(
 			<div>
-				{/* <img src="this.imgURL" alt="" /> */}
+				<img src={this.state.imageURLS[1]} alt="" />
 			</div>
 		);
 	}

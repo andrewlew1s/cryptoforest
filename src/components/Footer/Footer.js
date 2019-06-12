de9e08a933fe77ee5f88a79e58492367820c63cb
unknown <andrew12lewis@gmail.com>
2019-06-12 01:44:05 -0700

    chore: footer text

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import GitHub from '../../assets/github.svg'
 const footer = () => {
 	return (
 		<div className="footer">
-			<p>Thanks for visiting. The CryptoForest website is in early development phase and comments/improvements are always welcome. Feel free to check out the GitHub repo!</p>
+			<p>Thanks for visiting. The CryptoForest website is in early development. Suggestions and comments are always welcome. Feel free to check out the GitHub repo!</p>
 			<a href="https://github.com/andrewlew1s/cryptoforest" target="_blank">
 				<img height="22px" src={GitHub} alt=""/>
 			</a>

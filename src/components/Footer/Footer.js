4727c87c2046f1aee4daae970158393366257a3d
unknown <andrew12lewis@gmail.com>
2019-06-12 01:46:46 -0700

    chore: security

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ const footer = () => {
 	return (
 		<div className="footer">
 			<p>Thanks for visiting. The CryptoForest website is in early development. Suggestions and comments are always welcome. Feel free to check out the GitHub repo!</p>
-			<a href="https://github.com/andrewlew1s/cryptoforest" target="_blank">
+			<a href="https://github.com/andrewlew1s/cryptoforest" target="_blank" rel="noopener noreferrer">
 				<img height="22px" src={GitHub} alt=""/>
 			</a>
 		</div>
